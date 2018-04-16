da62b27a1b44e94726af52ea62b727eadefda73f
eco747 <etienne.cochard@gmail.com>
2018-04-16 14:06:43 +0200

    update colors

--- litehtml/perfect_hash.js
+++ litehtml/perfect_hash.js
@@ -384,3 +384,12 @@ generate( 'none;width;min-width;max-width;height;min-height;max-height;device-wi
 generate( 'content-box;border-box;',
 	'box_sizing', false, false  );
 
+generate( 'transparent;aliceblue;antiquewhite;aqua;aquamarine;azure;beige;bisque;black;blanchedalmond;blue;blueviolet;brown;burlywood;cadetblue;chartreuse;chocolate;coral;cornflowerblue;cornsilk;crimson;'+
+			'cyan;darkblue;darkcyan;darkgoldenrod;darkgray;darkgrey;darkgreen;darkkhaki;darkmagenta;darkolivegreen;darkorange;;darkorchid;darkred;darksalmon;darkseagreen;darkslateblue;darkslategray;darkslategrey;'+
+			'darkturquoise;darkviolet;deeppink;deepskyblue;dimgray;dimgrey;dodgerblue;firebrick;floralwhite;forestgreen;fuchsia;gainsboro;ghostwhite;gold;goldenrod;gray;grey;green;greenyellow;honeydew;hotpink;'+
+			'ivory;khaki;lavender;lavenderblush;lawngreen;lemonchiffon;lightblue;lightcoral;lightcyan;lightgoldenrodyellow;lightgray;lightgrey;lightgreen;lightpink;lightsalmon;lightseagreen;lightskyblue;lightslategray;'+
+			'lightslategrey;lightsteelblue;lightyellow;lime;limegreen;linen;magenta;maroon;mediumaquamarine;mediumblue;mediumorchid;mediumpurple;mediumseagreen;mediumslateblue;mediumspringgreen;mediumturquoise;mediumvioletred;'+
+			'midnightblue;mintcream;mistyrose;moccasin;navajowhite;navy;oldlace;olive;olivedrab;orange;orangered;orchid;palegoldenrod;palegreen;paleturquoise;palevioletred;papayawhip;peachpuff;peru;pink;plum;powderblue;'+
+			'purple;red;rosybrown;royalblue;saddlebrown;salmon;sandybrown;seagreen;seashell;sienna;silver;skyblue;slateblue;slategray;slategrey;snow;springgreen;steelblue;tan;teal;thistle;tomato;turquoise;violet;wheat;'+
+			'white;whitesmoke;yellow;yellowgreen;',
+	'web_colors', false, false );
